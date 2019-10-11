c0c342f671c33d25655bfc974525797af32ed148
marw12 <marwan.khan@mail.mcgill.ca>
2019-10-11 15:32:07 -0400

    upd

--- tutoringApp/src/test/java/com/ecse321/tutoringApp/TutoringAppApplicationTests.java
+++ tutoringApp/src/test/java/com/ecse321/tutoringApp/TutoringAppApplicationTests.java
@@ -1,16 +1,16 @@
 package com.ecse321.tutoringApp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class TutoringAppApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class TutoringAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	// @Test
+	// public void contextLoads() {
+	// }
 
-}
+//}
