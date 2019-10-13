bde6afee76bd80649e4352ae6ea25b13c1f29a82
marw12 <marwan.khan@mail.mcgill.ca>
2019-10-13 14:57:03 -0400

    app.java update

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplication.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplication.java
@@ -1,13 +1,13 @@
 package ca.mcgill.ecse321.tutoringapp;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-// public class TutoringappApplication {
+ @SpringBootApplication
+ public class TutoringappApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(TutoringappApplication.class, args);
-// 	}
+ 	public static void main(String[] args) {
+ 		SpringApplication.run(TutoringappApplication.class, args);
+ 	}
 
-// }
+ }
