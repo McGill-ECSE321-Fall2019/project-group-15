8c849222ab134608dd5dec319e798ef4de98c1bf
marw12 <marwan.khan@mail.mcgill.ca>
2019-10-13 15:46:12 -0400

    main update

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplication.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplication.java
@@ -2,12 +2,21 @@ package ca.mcgill.ecse321.tutoringapp;
 
  import org.springframework.boot.SpringApplication;
  import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.web.bind.annotation.RequestMapping;
 
+
+ @RestController
  @SpringBootApplication
  public class TutoringappApplication {
 
  	public static void main(String[] args) {
  		SpringApplication.run(TutoringappApplication.class, args);
  	}
+
+	@RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
  }
