5d9edc327010f2af465db25663d42d2e7d295354
marw12 <marwan.khan@mail.mcgill.ca>
2019-10-13 15:05:08 -0400

    apptest.java update

--- tutoringapp-backend/src/test/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplicationTests.java
+++ tutoringapp-backend/src/test/java/ca/mcgill/ecse321/tutoringapp/TutoringappApplicationTests.java
@@ -1,16 +1,16 @@
 package ca.mcgill.ecse321.tutoringapp;
 
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.junit4.SpringRunner;
+ import org.junit.Test;
+ import org.junit.runner.RunWith;
+ import org.springframework.boot.test.context.SpringBootTest;
+ import org.springframework.test.context.junit4.SpringRunner;
 
-// @RunWith(SpringRunner.class)
-// @SpringBootTest
-// public class TutoringappApplicationTests {
+ @RunWith(SpringRunner.class)
+ @SpringBootTest
+ public class TutoringappApplicationTests {
 
-// 	@Test
-// 	public void contextLoads() {
-// 	}
+ 	@Test
+ 	public void contextLoads() {
+ 	}
 
-// }
+ }
