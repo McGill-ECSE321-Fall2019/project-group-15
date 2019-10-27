b31b551ea53acd4d2736d263cd60493bfccfd887
Mich <mrmich12@gmail.com>
2019-10-27 16:21:57 -0400

    Added skeleton for Student dto/Tutor dto

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonDto.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonDto.java
@@ -1,5 +1,21 @@
 package ca.mcgill.ecse321.tutoringapp.dto;
 
+import java.util.Collections;
+import java.util.List;
+
 public class PersonDto {
+	private String name;
+
+	public PersonDto() {
+	}
+
+	public PersonDto(String name) {
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
 
 }
