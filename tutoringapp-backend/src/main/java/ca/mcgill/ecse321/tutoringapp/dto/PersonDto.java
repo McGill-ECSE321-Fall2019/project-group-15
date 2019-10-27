00214469feec75506d27f37faa7572a590996622
Darien Muse-Charbonneau <darienmusecharbonneau@gmail.com>
2019-10-27 16:51:56 -0400

    Update PersonDto. Create PersonController

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonDto.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonDto.java
@@ -1,5 +1,48 @@
 package ca.mcgill.ecse321.tutoringapp.dto;
 
+import java.util.Collections;
+import java.util.List;
+
 public class PersonDto {
 
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private List<PersonRoleDto> personRoles;
+	
+	public PersonDto() {
+	}
+	
+	@SuppressWarnings("unchecked")
+	public PersonDto(String firstName, String lastName, String userName) {
+		this(firstName, lastName, userName, Collections.EMPTY_LIST);
+	}
+	
+	public PersonDto(String firstName, String lastName, String userName, List<PersonRoleDto> arrayList) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.personRoles = arrayList;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public List<PersonRoleDto> getPersonRoles() {
+		return personRoles;
+	}
+	
+	public void setPersonRoles(List<PersonRoleDto> personRoles) {
+		this.personRoles = personRoles;
+	}
+	
 }
