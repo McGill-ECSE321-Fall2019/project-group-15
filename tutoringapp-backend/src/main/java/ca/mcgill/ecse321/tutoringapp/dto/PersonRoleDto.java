8a67984f0414cba400e851f3f1a3a44b7e231524
Darien Muse-Charbonneau <darienmusecharbonneau@gmail.com>
2019-10-27 17:00:24 -0400

    Update PersonRoleDto

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonRoleDto.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/PersonRoleDto.java
@@ -1,5 +1,32 @@
 package ca.mcgill.ecse321.tutoringapp.dto;
 
 public class PersonRoleDto {
-
+	
+	private String password;
+	private PersonDto person;
+	
+	public PersonRoleDto() {
+	}
+	
+	public PersonRoleDto(String password) {
+		this(password, null);
+	}
+	
+	public PersonRoleDto(String password, PersonDto person) {
+		this.password = password;
+		this.person = person;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public PersonDto getPerson() {
+		return person;
+	}
+	
+	public void setPerson(PersonDto person) {
+		this.person = person;
+	}
+	
 }
