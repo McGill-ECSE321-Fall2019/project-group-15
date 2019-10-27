e882cd5d2d564ac7eaea3593c5e568b171198dbf
Darien Muse-Charbonneau <darienmusecharbonneau@gmail.com>
2019-10-27 17:09:09 -0400

    Update TutorDto.java

--- tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/TutorDto.java
+++ tutoringapp-backend/src/main/java/ca/mcgill/ecse321/tutoringapp/dto/TutorDto.java
@@ -2,4 +2,36 @@ package ca.mcgill.ecse321.tutoringapp.dto;
 
 public class TutorDto {
 
+	private Float hourlyRate;
+	private Boolean isVerified;
+	//Inherited from PersonRoleDto
+	private String password;
+	private PersonDto person;
+	
+	public TutorDto(Float hourlyRate) {
+		this(hourlyRate, false, null);
+	}
+	
+	public TutorDto(Float hourlyRate, Boolean isVerified, PersonDto person) {
+		this.hourlyRate = hourlyRate;
+		this.isVerified = isVerified;
+		this.person = person;
+	}
+	
+	public Float getHourlyRate() {
+		return hourlyRate;
+	}
+	
+	public Boolean getIsVerified() {
+		return isVerified;
+	}
+	
+	public PersonDto getPerson() {
+		return person;
+	}
+	
+	public void setPerson(PersonDto person) {
+		this.person = person;
+	}
+	
 }
