1d931121e647f9c79bdbf1e0483dfc96ef0ef7b7
Mich Riendeau <41454040+michr12@users.noreply.github.com>
2019-11-07 17:00:26 -0500

    Update index.js

--- tutoringapp-frontend/src/router/index.js
+++ tutoringapp-frontend/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'tutoringApp',
+      component: tutoringApp
     }
   ]
 })
