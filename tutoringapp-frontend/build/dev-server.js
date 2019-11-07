1ea2d33260a958229ee2d43f88013fde7b2d81a8
Mich Riendeau <41454040+michr12@users.noreply.github.com>
2019-11-07 16:41:34 -0500

    Update dev-server.js

--- tutoringapp-frontend/build/dev-server.js
+++ tutoringapp-frontend/build/dev-server.js
@@ -65,7 +65,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+//var uri = 'http://localhost:' + port commented as seen in tutorial -MIch
 
 var _resolve
 var readyPromise = new Promise(resolve => {
