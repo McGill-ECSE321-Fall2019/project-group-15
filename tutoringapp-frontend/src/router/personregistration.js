93955d4a2f1362cf688cda2f49e4618d37cf7116
Osman Warsi <osman.warsi@live.com>
2019-11-12 16:33:32 -0500

    Added a JS file for data event handler for PersonDto class

---
+++ tutoringapp-frontend/src/router/personregistration.js
@@ -0,0 +1,43 @@
+//Testing this out right now
+//Author : Osman
+
+//Dtos
+function PersonDto(firstName, lastName, userName) {
+this.firstName = firstName
+this.lastName = lastName
+this.userName = userName
+}
+
+//data variables
+export default {
+name: 'personregistration',
+data () {
+	return {
+		people: [],
+		newPerson: '',
+		errorPerson: '',
+		response: []
+		}
+	},
+	//....
+}
+
+//Initialization function
+created: function() {
+	//Test data
+	const p1 = new PersonDto('Osman')
+	const p2 = new PersonDto('Marwan')
+	//Sample initial content
+	this.people = [p1,  p2]
+},
+
+//event handling method
+methods: {
+	createPerson: function (personName) {
+	//Create a new person and add it to the list of people
+	var p = new PersonDto(personName)
+	this.people.push(p)
+	//Reset the name field for the new people
+	this.newPerson = ''
+	}
+}
