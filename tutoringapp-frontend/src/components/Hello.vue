1daf4633f97a8495a6c19f61e461f06d81f9f9fc
Osman Warsi <osman.warsi@live.com>
2019-11-13 18:16:02 -0500

    Testing new changes to localhost view

--- tutoringapp-frontend/src/components/Hello.vue
+++ tutoringapp-frontend/src/components/Hello.vue
@@ -25,7 +25,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your ECSE 321 Group 15 - Tutoring System'
     }
   }
 }
