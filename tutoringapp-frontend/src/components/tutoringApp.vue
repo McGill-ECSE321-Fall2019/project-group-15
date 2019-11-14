0fce836a1856d3b16ba700921bfba04c1c936c58
Mich <mrmich12@gmail.com>
2019-11-14 14:21:21 -0500

    Added vue

--- tutoringapp-frontend/src/components/tutoringApp.vue
+++ tutoringapp-frontend/src/components/tutoringApp.vue
@@ -1,6 +1,20 @@
 <template>
+<div class="row">
+    <div class="col-sm">
+         Name
+    </div>
+    <div class="col-sm">
+        <p class="title">ID</p>
+    </div>
+    <div class="col-sm">
+Rating
+    </div>
+    <div
+        class="col-sm">
+        <button>Delete</button>
+    </div>
+</div>
 </template>
+
 <script>
 </script>
-<style>
-</style>
