05def53f10920d7b6ae7260bd2c30b205bad82d8
Darien Muse-Charbonneau <darienmusecharbonneau@gmail.com>
2019-11-14 19:07:37 -0500

    Add tutorRegistration.js and Tutor.vue

---
+++ tutoringapp-frontend/src/components/javascript/tutorRegistration.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+//Dtos
+function TutorDto(firstName, lastName, id, hourlyRate, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.hourlyRate = hourlyRate;
+    this.password = password;
+    }
+
+//data variables
+export default {
+    name: 'personregistration',
+    data () {
+        return {
+            people: [],
+            newPerson: '',
+            errorPerson: '',
+            response: []
+            }
+        },
+created: function () {
+    // Initializing people from backend
+      AXIOS.get(`/tutors`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.tutors = response.data
+      })
+      .catch(e => {
+        this.errorTutor = e;
+      });
+  }
+}
