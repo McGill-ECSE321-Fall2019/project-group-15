417b1c1fd139d513c68bd4ae1a9c037bcd835910
marw12 <marwan.khan@mail.mcgill.ca>
2019-11-17 18:38:48 -0500

    course.vue update

--- tutoringapp-frontend/src/components/Course.vue
+++ tutoringapp-frontend/src/components/Course.vue
@@ -1,7 +1,5 @@
 <template>
-
   <div class="course">
-      
     <!-- Navbar -->
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
@@ -21,82 +19,64 @@
         </b-collapse>
       </b-navbar>
     </div>
-    <!-- Header -->
-    <h1>Manage Rooms</h1>
-    <!-- Search -->
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="./#/Dashboard">Dashboard</b-nav-item>
-            <b-nav-item href="./#/Tutor">Tutors</b-nav-item>
-            <b-nav-item href="./#/Room">Rooms</b-nav-item>
-            <b-nav-item href="./#/Course">Courses</b-nav-item>
-            <b-nav-item href="./#/Evaluation">Evaluations</b-nav-item>
-            <b-nav-item href="./#/Student">Students</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-
-
-    <!-- Table -->
-     <div id="table">
-                    <table class="info-table" style="width: 100%; height: 100%;">
-                        
-                        <tr v-for="(course, i) in courses" v-bind:key="`course-${i}`">
-                            <td style="padding:10px">
-                            </td>
-                            <td style="padding:10px">{{ course.name }}</td>
-                            <td style="padding:10px">{{ course.description }}</td>
-                            
-                        </tr>
-                        <tr>
-                            <th style="padding:10px">Course Name</th>
-                            <th style="padding:10px">Course Description</th>
-                        </tr>
-                    </table>
-					<h6 align="left" style="padding-top:1.6%">Add Course</h6>
-                </div>
-
-
-    <!-- Input Table-->
+   
+      
+    <!-- Tutorial Input Table -->
     <table>
-        <tr>
-            <td>
-                <input type="text" v-model="newCourse" placeholder="Enter Course Name">
-            </td>
-            <td>
-                <input type="text" v-model="newDescription" placeholder="Enter Description">
-            </td>
-            <td>
-                <button @click="createCourse(newCourse, newDescription)">Create Course</button>
-            </td>
-        </tr>
-    </table>
+  <tr>
+    <td>
+        <input type="text" v-model="newCourse" placeholder="Course Name">
+    </td>
+    <td>
+        <input type="text" v-model="newDescription" placeholder="Course Description">
+    </td>
+    <td>
+        <button @click="createCourse(newCourse,newDescription)">Create Course</button>
+    </td>
+  </tr>
+</table>
+
     <br />
+    
+    <!-- Bootstrap Table -->
+    <div>
+    <b-table :items="tutors" :fields="['Course Name' , 'Course Description']">
+      
+      <!-- Name Column -->
+      <template v-slot:cell(name)="data">
+        {{ data.item.person.firstName }} {{ data.item.person.lastName }}
+      </template>
+
+      <!-- Status Column -->
+      <template v-slot:cell(status)="data">
+          <b-form-checkbox
+            v-model="data.item.status"
+            checked="data.item.isVerified"
+            @change="toggleIsVerified(data.item)"
+            value="Approved"
+            unchecked-value="Unapproved"
+          >
+            <strong>{{ data.item.status }}</strong>
+          </b-form-checkbox>
+        </template>
+
+      <!-- Delete Column -->
+      <template v-slot:cell(delete)="data">
+          <b-button 
+          @click="removeTutor(data.item)"
+          variant="danger">
+          Delete</b-button>
+        </template>
+      
+    </b-table>
+    </div>
 
   </div>
-  
 </template>
 
 <style>
-body {
-  font-family: Century Gothic;
-
-}
-
-h1, h2, h3 {
-  font-weight: normal;
-  font-family: Century Gothic;
-}
-
-.room {
-    width: 100%;
-}
-.sign-up {
-	width: 70%;
-	text-align: left;
-	margin-left: 50px;
-}
 </style>
 
-<script src="./javascript/course.js">
+<script src="./javascript/tutorRegistration.js">
 </script>
